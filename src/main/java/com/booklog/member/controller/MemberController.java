2a719fe49745a147029790aa2c6d3b60515b4aa8
joonsuk12 <1004joonsuk@naver.com>
2023-05-31 17:49:22 +0900

    5/31 17:49 swagger setting done, RDS connection success

--- src/main/java/com/booklog/member/controller/MemberController.java
+++ src/main/java/com/booklog/member/controller/MemberController.java
@@ -2,6 +2,11 @@ package com.booklog.member.controller;
 
 
 import com.booklog.member.model.MemberDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "회원 api")
 @RestController
 @RequestMapping("/member")
 public class MemberController {
@@ -23,6 +29,11 @@ public class MemberController {
     }
 
     @PostMapping("/regist")
+    @ApiOperation(value = "Form으로 입력받은 회원 정보를 토대로 회원가입")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful"),
+            @ApiResponse(code = 404, message = "Attractions not found")
+    })
     public ResponseEntity<String> regist(@RequestBody MemberDto memberDto) throws Exception {
         System.out.println(memberDto);
         return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
